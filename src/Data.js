3cdabc4db6a0fa2301a77049758af41d98c1e61e
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-04-12 16:10:45 +0530

    add hamburger-menu styles and functionality

--- src/Data.js
+++ src/Data.js
@@ -29,3 +29,37 @@ export const Data = [
         className : 'data-items'
     }
 ]
+
+ export const menuData = [
+    {
+       text:'Home',
+       path: '/',
+       className : 'menu-data'
+    },
+    {
+        text:'Gallery',
+        path : '/gallery',
+        className : 'menu-data'
+     },
+     {
+        text:'Sign In',
+        path : '/signin',
+        className : 'menu-data'
+     },
+     {
+        text:'Sign Up',
+        path:'/signup',
+        className : 'menu-data'
+     },
+     {
+        text:'About',
+        path:'/about',
+        className : 'menu-data'
+     },
+     {
+        text:'History',
+        path:'/history',
+        className : 'menu-data'
+     },
+
+]
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,57 @@
 import { MenuOutlined } from '@material-ui/icons';
-import React from 'react';
+import React, { useState } from 'react';
 // import { Link } from 'react-router-dom';
 import './styles/Header.scss';
+import { menuData } from '../Data';
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
-    <header className='header'>
+    <div className='header'>
       <nav className='nav-container'>
         <div className='menu'>
-          <MenuOutlined className='menu-icon' />
+          <MenuOutlined className='menu-icon' onClick={toggleMenu} />
         </div>
 
         <div className='logo'>
           <h1>Etravel</h1>
         </div>
 
-        <div className="right">
+        <div className='right'>
           <ul>
-            <li><a href="/places">Places</a></li>
-            <li><a href="/pricing">Pricing</a></li>
-            <li><a href="/signup">Collections</a></li>
-            <li><a href="/sponsors">Sponsors</a></li>
+            <li>
+              <a href='/places'>Places</a>
+            </li>
+            <li>
+              <a href='/pricing'>Pricing</a>
+            </li>
+            <li>
+              <a href='/signup'>Collections</a>
+            </li>
+            <li>
+              <a href='/sponsors'>Sponsors</a>
+            </li>
           </ul>
         </div>
       </nav>
-    </header>
+
+      <nav className={openMenu ? 'slider active' : 'slider'}>
+        <ul onClick={toggleMenu} className='ul-menu'>
+          {menuData.map((item, idx) => (
+            <div className='container' key={idx}>
+              <li className={item.className}>
+                <a href={item.path}>{item.text}</a>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
